9d56f0109491df9a366488b46a1ca6d524abb778
Joseph March <josephmarch70@gmail.com>
2026-09-23 16:00:02 +0100

    protestors and popups!!!! :P

--- script.js
+++ script.js
@@ -10,3 +10,61 @@ closePopup.addEventListener("click", function() {
 
     music.play();
 });
+
+const padlock = document.getElementById("padlock");
+const lockPopup = document.getElementById("lock-popup");
+const lockSound = document.getElementById("lock-sound");
+const closeLock = document.getElementById("close-lock");
+const submitCode = document.getElementById("submit-code");
+const codeInput = document.getElementById("code-input");
+const codeMessage = document.getElementById("code-message");
+
+padlock.volume = 10;
+
+padlock.addEventListener("click", function() {
+
+    lockSound.currentTime = 0;
+    lockSound.play();
+
+    lockPopup.style.display = "flex";
+
+    codeInput.focus();
+
+});
+
+closeLock.addEventListener("click", function() {
+
+    lockPopup.style.display = "none";
+
+    codeInput.value = "";
+    codeMessage.textContent = "";
+
+});
+
+submitCode.addEventListener("click", function() {
+
+    const enteredCode = codeInput.value;
+
+    if (enteredCode === "1234") {
+
+        window.location.href = "datacentre.html";
+
+    } else {
+
+        codeMessage.textContent = "INCORRECT ACCESS CODE.";
+
+    }
+
+});
+
+const protestorsButton = document.getElementById("protestors");
+const protestorsDialogue = document.getElementById("protest-speech");
+const protestorsDialogueClose = document.getElementById("close-speech");
+
+protestorsButton.addEventListener("click", function() {
+    protestorsDialogue.style.display = "flex";
+});
+
+protestorsDialogueClose.addEventListener("click", function() {
+    protestorsDialogue.style.display = "none";
+});
